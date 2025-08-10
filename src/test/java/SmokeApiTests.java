3953f7476527b1108802e9eea5a3793b13b4d580
Evgenii Saganchi <esaganchi@gmail.com>
2025-08-10 14:42:13 +0200

    Add basic API test

--- src/test/java/SmokeApiTests.java
+++ src/test/java/SmokeApiTests.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-public class ApiTests {
+import static io.restassured.RestAssured.given;
+
+public class SmokeApiTests {
+    @Test
+    void simpleTest () {
+        // AAA - Arrange - Act -Assert
+        String body = """
+                {
+                  "id": 0,
+                  "username": "string",
+                  "firstName": "string",
+                  "lastName": "string",
+                  "email": "string",
+                  "password": "string",
+                  "phone": "string",
+                  "userStatus": 0
+                }""";
+
+        Response response = given()
+                    .header("accept", "application/json")
+                    .header("Content-Type", "application/json")
+                    .baseUri("https://petstore.swagger.io/v2/")
+                .when()
+                    .body(body)
+                    .post("user")
+                .andReturn();
+        int actualCode = response.getStatusCode();
+
+        Assertions.assertEquals(200, response.getStatusCode());
+    }
 }
